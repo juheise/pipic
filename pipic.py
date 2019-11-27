592bfd9b4fda3d32e92d1afb24cd4f8486dceff2
Julian Heise <julian.heise@zalando.de>
2019-11-27 17:58:26 +0100

    added random input option

--- pipic.py
+++ pipic.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
+from random import Random
 
 from PIL import Image
 import argparse
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument("--width")
-argparser.add_argument("--height")
-argparser.add_argument("--outfile", default="pi.png")
-argparser.add_argument("--infile", default="pi.txt")
-args = argparser.parse_args()
 
-width = int(args.width)
-height = int(args.height)
+class RandomInput:
 
-max_cursor = width * height
+    def __init__(self):
+        self.generator = Random()
 
-img = Image.new('RGB', (width, height))
-cursor = 0
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+    def read(self, _):
+        return int(self.generator.random() * 10 + 0.5)
 
 
 def draw_pixel(img, cursor, digit, height):
@@ -26,11 +27,35 @@ def draw_pixel(img, cursor, digit, height):
     img.putpixel((x,y), (shade, shade, shade))
 
 
-with open(args.infile) as f:
-    while cursor < max_cursor:
-        char = f.read(1)
-        digit = int(char)
-        draw_pixel(img, cursor, digit, height)
-        cursor += 1
+def input_stream(random=False, filename=None):
+    if random:
+        return RandomInput()
+    return open(filename)
 
-img.save(args.outfile)
+
+if __name__ == "__main__":
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--width")
+    arg_parser.add_argument("--height")
+    arg_parser.add_argument("--outfile", default="pi.png")
+    arg_parser.add_argument("--infile", default="pi.txt")
+    arg_parser.add_argument("--random", action="store_true")
+    args = arg_parser.parse_args()
+
+    width = int(args.width)
+    height = int(args.height)
+
+    max_cursor = width * height
+
+    img = Image.new('RGB', (width, height))
+    cursor = 0
+
+    with input_stream(filename=args.infile, random=args.random) as f:
+        while cursor < max_cursor:
+            char = f.read(1)
+            digit = int(char)
+            draw_pixel(img, cursor, digit, height)
+            cursor += 1
+
+    img.save(args.outfile)
