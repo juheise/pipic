2a89907eafd30b85552b3161471edcaf6601a669
Julian Heise <julian.heise@zalando.de>
2019-11-28 12:43:16 +0100

    fixed issue with drawing piseq

--- img_io/__init__.py
+++ img_io/__init__.py
@@ -17,6 +17,9 @@ def save_image(img, path):
 
 
 def buffer_to_image(buffer, width):
+    max_shade = 0
+    for b in buffer:
+        max_shade = max(b, max_shade)
     height = int(len(buffer) / width) +1
     img = new_image(width, height)
     x = 0
@@ -25,7 +28,7 @@ def buffer_to_image(buffer, width):
         if x == width:
             x = 0
             y += 1
-        shade = int(b)
+        shade = int(b/max_shade * 255)
         img.putpixel((x,y), (shade, shade, shade))
         x += 1
     return img
